22d75ce1c3afc5bab0de685fa47b3b4570639966
lujin112 <lujin11j@gmail.com>
2025-12-03 17:57:33 +0300

    Refactor ProviderRegister class formatting
    Removed unnecessary indentation and adjusted comment formatting.

--- schemas/provider_schema.py
+++ schemas/provider_schema.py
@@ -2,12 +2,10 @@ from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 
 
-
-    class ProviderRegister(BaseModel):
+class ProviderRegister(BaseModel):
     name: str = Field(..., min_length=2)
 
-
-    # ✅ واحد فقط من الاثنين (إيميل أو هاتف)
+    # واحد فقط من الاثنين (إيميل أو هاتف)
     email: Optional[EmailStr] = None
     phone: Optional[str] = Field(default=None, min_length=10, max_length=10)
 
@@ -16,7 +14,6 @@ from typing import Optional
     address: str = Field(..., min_length=5)
 
     description: Optional[str] = None
-
     password: str = Field(..., min_length=8)
 
 
